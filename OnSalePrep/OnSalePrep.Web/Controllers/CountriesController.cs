1af9793abacf0bc160eb6044d56fa2f5e06ae10e
rafaelsantos1000 <rafaasfs@gmail.com>
2025-01-27 15:40:53 +0000

    Finish countries CRUD

--- OnSalePrep/OnSalePrep.Web/Controllers/CountriesController.cs
+++ OnSalePrep/OnSalePrep.Web/Controllers/CountriesController.cs
@@ -14,13 +14,13 @@ namespace OnSalePrep.Web.Controllers
             _context = context;
         }
 
-        // GET: Countries
+       
         public async Task<IActionResult> Index()
         {
             return View(await _context.Countries.ToListAsync());
         }
 
-        // GET: Countries/Details/5
+        
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -38,29 +38,46 @@ namespace OnSalePrep.Web.Controllers
             return View(country);
         }
 
-        // GET: Countries/Create
+        
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Countries/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name")] Country country)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(country);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                try
+                {
+                    _context.Add(country);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch(DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Já existe um país com o mesmo nome!");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch(Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+                
             }
             return View(country);
         }
 
-        // GET: Countries/Edit/5
+        
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -76,9 +93,7 @@ namespace OnSalePrep.Web.Controllers
             return View(country);
         }
 
-        // POST: Countries/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Country country)
@@ -94,24 +109,29 @@ namespace OnSalePrep.Web.Controllers
                 {
                     _context.Update(country);
                     await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (DbUpdateException dbUpdateException)
                 {
-                    if (!CountryExists(country.Id))
+                    if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
                     {
-                        return NotFound();
+                        ModelState.AddModelError(string.Empty, "Já existe um país com o mesmo nome!");
                     }
                     else
                     {
-                        throw;
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+                
             }
             return View(country);
         }
 
-        // GET: Countries/Delete/5
+        
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -129,7 +149,7 @@ namespace OnSalePrep.Web.Controllers
             return View(country);
         }
 
-        // POST: Countries/Delete/5
+        
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -144,9 +164,5 @@ namespace OnSalePrep.Web.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CountryExists(int id)
-        {
-            return _context.Countries.Any(e => e.Id == id);
-        }
     }
 }
